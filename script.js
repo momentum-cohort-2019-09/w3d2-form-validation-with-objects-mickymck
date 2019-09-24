f3e0871aefba9d64a5632118fd046ce1bcbc9cb5
mickymck <mickymckeon@gmail.com>
2019-09-24 15:01:43 -0400

    started on field validation

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 console.log('Add validation!')
+
+function query(selector) {
+    return document.querySelector(selector)
+}
+
+function querys(selector) {
+    return document.querySelectorAll(selector)
+}
+
+class Field {
+    constructor(input) {
+        this.input = input;
+    }
+    fieldIsNotEmpty () {
+        return this.input !== ""
+    }
+    isNumber () {
+        return !isNaN(this.input)
+    }
+
+    isCVV () {
+        return this.input.length === 3
+    }
+
+}
+
+query("#parking-form").addEventListener("submit", function (event) {
+    event.preventDefault()
+
+let nameField = new Field (query("#name").value.trim())
+
+let daysField = new Field (query("#days").value.trim())
+
+let cvvField = new Field (query("#cvv").value.trim())
+
+nameField.fieldIsNotEmpty()
+
+daysField.fieldIsNotEmpty()
+
+daysField.isNumber()
+
+cvvField.isNumber()
+
+cvvField.isCVV()
+
+})
+
