a70e05ba41c3a0d1ad993cda74ef2d27a49efa57
mickymck <mickymckeon@gmail.com>
2019-09-24 21:44:47 -0400

    really struggling tonight

--- script.js
+++ script.js
@@ -8,41 +8,103 @@ function querys(selector) {
     return document.querySelectorAll(selector)
 }
 
+class Form {
+    constructor(fieldDescription) {
+        this.nameField = nameField
+        this.carField = carField
+        this.dateField = dateField
+        this.daysField = daysField
+        this.creditCardField = creditCardField
+        this.cvvField = cvvField
+        this.expirationField = expirationField;
+    }
+
+    markValid () {
+        this.field.classList.remove("input-invalid")
+        this.field.classList.add("input-valid")
+    }
+
+    markInvalid () {
+        this.field.classList.remove("input-valid")
+        this.field.classList.add("input-invalid")
+
+        const errorField = document.createElement("p")
+        errorField.classList.add("input-hint", "text-danger", "error-message")
+        errorField.innerText = "invalid"
+        this.field.appendChild(errorField)
+    }
+
+    // calculate total
+}
+
 class Field {
     constructor(input) {
         this.input = input;
     }
-    fieldIsNotEmpty () {
+
+    fieldIsNotEmpty() {
         return this.input !== ""
     }
-    isNumber () {
+
+    isNumber() {
         return !isNaN(this.input)
     }
 
-    isCVV () {
+    isCVV() {
         return this.input.length === 3
     }
 
+    isFutureDate() {
+        let now = new Date()
+        now.setUTCHours(0, 0, 0, 0)
+        let parkDate = new Date(this.date)
+        parkDate.setUTCHours(0, 0, 0, 0)
+        return parkDate >= now
+    }
 }
 
 query("#parking-form").addEventListener("submit", function (event) {
     event.preventDefault()
 
-let nameField = new Field (query("#name").value.trim())
+    let nameField = new Field(query("#name").value)
 
-let daysField = new Field (query("#days").value.trim())
+    let carField = new Field(query("#car-info").value)
 
-let cvvField = new Field (query("#cvv").value.trim())
+    let dateField = new Field(query("#start-date").value)
 
-nameField.fieldIsNotEmpty()
+    let daysField = new Field(query("#days").value)
 
-daysField.fieldIsNotEmpty()
+    let creditCardField = new Field(query("#credit-card").value)
 
-daysField.isNumber()
+    let cvvField = new Field(query("#cvv").value)
 
-cvvField.isNumber()
+    let expirationField = new Field(query("#expiration").value)
 
-cvvField.isCVV()
+    if (nameField.fieldIsNotEmpty()) {
+        nameField.markValid ()
+    } else {
+        nameField.markInvalid ()
+    }
+
+    carField.fieldIsNotEmpty()
+
+    dateField.fieldIsNotEmpty()
+
+    daysField.fieldIsNotEmpty()
+
+    creditCardField.fieldIsNotEmpty()
+
+    cvvField.fieldIsNotEmpty()
+
+    expirationField.fieldIsNotEmpty()
+
+    dateField.isFutureDate()
+
+    daysField.isNumber()
+
+    cvvField.isNumber()
+
+    cvvField.isCVV()
 
 })
 
