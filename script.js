8087db41eb1bc7d6ddec7cebda2eebdc18aa6dd4
mickymck <mickymckeon@gmail.com>
2019-09-25 21:47:49 -0400

    for Stephen

--- script.js
+++ script.js
@@ -14,79 +14,69 @@ class Validator {
         this.errorMsg = errorMsg
     }
 
-    validate (trueOrFalse) {
-        return this.test (trueOrFalse)
+    validate (value) {
+        return this.test (value)
     }
 }
 
 class Field {
-    constructor(fieldDiv, input) {
-        this.fieldDiv = fieldDiv
-        this.input = input;
+    constructor(inputDiv, validations) {
+        this.inputDiv = inputDiv
+        this.validations = validations || [];
     }
 
-    markValid() {
-        this.fieldDiv.classList.remove("input-invalid")
-        this.fieldDiv.classList.add("input-valid")
+    theBigGetter() {
+        let input = query("#name")
+        let value = input.value
+        return value
     }
 
-    markInvalid() {
-        this.fieldDiv.classList.remove("input-valid")
-        this.fieldDiv.classList.add("input-invalid")
+    validateField () {
+        const value = this.theBigGetter()
+        let errorMsgs = []
+        for (let validation of validations) {
+            
+        }
     }
 
+    // markValid() {
+    //     this.fieldDiv.classList.remove("input-invalid")
+    //     this.fieldDiv.classList.add("input-valid")
+    // }
 
-}
+    // markInvalid() {
+    //     this.fieldDiv.classList.remove("input-valid")
+    //     this.fieldDiv.classList.add("input-invalid")
+    // }
 
     // mark each field as valid
 
     // provide a total for the user, based on the date and days fields
 
+
+}
+
+let fieldIsNotEmpty = new Validator(input => !!input, "this field must not be empty")
+
+
+
+let name = new Field(query("#name-field"), [fieldIsNotEmpty])
+
+// let car = new Field(query("#car-field"), [fieldIsNotEmpty])
+
+// let date = new Field(query("#date-field"), [fieldIsNotEmpty])
+
+// let days = new Field(query("#days-field"), [fieldIsNotEmpty])
+
+// let creditCard = new Field(query("#creditCard-field"), [fieldIsNotEmpty])
+
+// let cvv = new Field(query("#cvv-field"), [fieldIsNotEmpty])
+
+// let expiration = new Field(query("#expiration-field"), [fieldIsNotEmpty])
+
+
+
 query("#parking-form").addEventListener("submit", function (event) {
     event.preventDefault()
-
-    let name = new Field(query("#name-field"), query("#name").value)
-
-    let car = new Field(query("#name-field"), query("#car-info").value)
-
-    let date = new Field(query("#name-field"), query("#start-date").value)
-
-    let days = new Field(query("#name-field"), query("#days").value)
-
-    let creditCard = new Field(query("#name-field"), query("#credit-card").value)
-
-    let cvv = new Field(query("#name-field"), query("#cvv").value)
-
-    let expiration = new Field(query("#name-field"), query("#expiration").value)
-
-    console.group(name.fieldIsNotEmpty())
-
-    car.fieldIsNotEmpty()
-
-    date.fieldIsNotEmpty()
-
-    days.fieldIsNotEmpty()
-
-    creditCard.fieldIsNotEmpty()
-
-    cvv.fieldIsNotEmpty()
-
-    expiration.fieldIsNotEmpty()
-
-    // date.isFutureDate()
-
-    days.isNumber()
-
-    cvv.isNumber()
-
-    cvv.isCVV()
-
 })
 
-let fieldIsNotEmpty = new Validator ( , "this field must not be empty")
-
-let isNumber = new Validator ( , "this field must be a number")
-
-let isCVV = new Validator ( , "CVV must be 3 digits")
-
-let isFutureDate = new Validator ( , "date must not be in the past")
